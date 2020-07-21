40303bfe3ef6c17bd20a2b851fc89af98099e9e9
crw-ethz <cedric.r.weber@me.com>
2020-07-21 17:30:24 +0200

    Add files via upload

--- immuneREF_tutorial.R
+++ immuneREF_tutorial.R
@@ -2,20 +2,22 @@
 # 01 Main script to preprocess repertoires, calculate characteristics and calculate correlation matrix
 #
 library(immuneREF)
-library(foreach)
-library(doMC)
-registerDoMC(1)
 
-# Set working directory
-setwd("/Users/cedricweber/Desktop/tutorial_immuneREF")
+## While not used in this tutorial, repertoire analysis and similarity score calculation can be parallelized (see readthedocs) 
+#library(foreach)
+#library(doParallel)
+#registerDoParallel(1)
 
-# Load datasets (check for compatibility, 
-# Compatible repertoires have: 
-#       1) AIRR standard column naming. 
-#       2) V(D)J naming compatible with included list_germline_genes dataset.
+
+#Set working directory
+setwd(PATH)
+
+# Load datasets (check for compatibility,
+# Compatible repertoires have:
+#       1) AIRR standard column naming.
+#       2) VJ naming compatible with the included list_germline_genes dataset.
 #       3) Clonal frequencies that sum up to 1.
 list_simulated_repertoires <- tutorial_repertoires
-
 
 # Set number of repertoires to be analyzed and names for reference dataframe
 repertoire_names <- names(list_simulated_repertoires)
@@ -24,11 +26,13 @@ repertoire_species <- sapply(strsplit(repertoire_names, "\\_"),function(x) x[[1]
 repertoire_receptor <- sapply(strsplit(repertoire_names, "\\_"),function(x) x[[2]])
 repertoire_chain <- sapply(strsplit(repertoire_names, "\\_"),function(x) x[[3]])
 
+# Create dataframe containing all relevant metadata on repertoires
 input_data_ref<-data.frame(sample_id = repertoire_names,
                            nb_sequences = repertoire_lengths,
                            species = repertoire_species,
                            receptor = repertoire_receptor,
                            chain = repertoire_chain,row.names=c(1:length(repertoire_names)))
+
 
 
 
@@ -41,8 +45,9 @@ overlap_layer<-repertoire_overlap(list_simulated_repertoires,basis="CDR3_aa")
 subsample_size<-10000
 
 list_simulated_repertoires<-subset_input_repertoires(list_repertoires=list_simulated_repertoires,
-                               subset_size=subsample_size,
-                               random=FALSE)
+                           subset_size=subsample_size,
+                           random=FALSE)
+
 
 
 # Calculate all features for each repertoire
@@ -63,17 +68,22 @@ list_single_layers<-calculate_similarities(
   overlap_layer=overlap_layer)
 
 
-
-
 # Calculate condensed network (here equal weights for each layer)
 cormat <- condense_layers(list_single_layers,
     weights = c(1,1,1,1,1,1),
     method = "standard")
 
 
+
+
 ###
 # Draw heatmap of immuneREF layers
 ###
+
+
+# Create folder to save figures in
+dir.create("figures")
+
 
 # Make list of all layers you want to plot heatmaps for
 list_all_layers <- list_single_layers
@@ -90,12 +100,9 @@ annotation_list[["colors"]]<-list(Species=c(mm='#ffffbf',hs='#fc8d59'),
 
 
 # For each entry (immuneREF layer) plot a heatmap
-# Note: Folder for which path is set in path_figure needs to exist already
 print_heatmap_sims(list_similarity_matrices=list_all_layers,
                   annotation_list=annotation_list,
                   path_figure="figures")
-
-
 
 
 ###
@@ -108,13 +115,10 @@ categories_list[["categories"]]<-input_data_ref
 categories_list[["color"]]<-c("white",'#91bfdb','#ffffbf')
 categories_list[["subset"]]<-"species"
 
-#Plot global similarity 
+#Plot global similarity
 print_global_similarity(list_similarity_matrices=list_all_layers,
                       categories_list = categories_list,
                       path_figure="figures")
-
-
-
 
 ###
 # Plot local similarity per category and identify max and min locally similar repertoires
@@ -127,24 +131,27 @@ max_min_reps<-print_local_similarity(list_similarity_matrices=list_all_layers,
 ###
 # Radar plot to visualize similarity across all 6 layers
 ##
-
 radar_list<-list()
-radar_list[["category"]]<-c("Murine A","Murine B","Human A","Human B")
-radar_list[["roi"]]<-c("mm_ig_h_2_0__0_0_0_A","mm_ig_h_4_0__0_0_0_A","hs_ig_h_2_0__0_0_0_A","hs_ig_h_4_0__0_0_0_A")
-radar_list[["label"]]<-c("Murine A","Murine B","Human A","Human B")
-radar_list[["colors"]]<-c("grey","blue",'red',"green")
+radar_list[["mm_ig_h_2_0__0_0_0_A"]]<-repertoires_analyzed[["mm_ig_h_2_0__0_0_0_A"]]
+radar_list[["mm_ig_h_4_0__0_0_0_A"]]<-repertoires_analyzed[["mm_ig_h_4_0__0_0_0_A"]]
+radar_list[["hs_ig_h_2_0__0_0_0_A"]]<-repertoires_analyzed[["hs_ig_h_2_0__0_0_0_A"]]
+radar_list[["hs_ig_h_4_0__0_0_0_A"]]<-repertoires_analyzed[["hs_ig_h_4_0__0_0_0_A"]]
 
-comparison_list<-list(roi=radar_list[["roi"]],
-  roi_names=radar_list[["category"]],
+comparison_list<-list(roi=names(radar_list),
+  roi_names=c(
+    "Murine A",
+    "Murine B",
+    "Human A",
+    "Human B"),
   ref="mm_ig_h_2_0__0_0_0_A",
-  plot_names=radar_list[["label"]],
-  colors=radar_list[["colors"]])
+  plot_names=c("Murine A", "Murine B","Human A","Human B"),
+  colors=c("grey","blue",'red',"green"))
+
 
 print_repertoire_radar(list_similarity_matrices=list_single_layers,
   to_compare=comparison_list,
   path_figure="figures",
   name_plot="tutorial")
-
 
 
 ####
@@ -154,7 +161,9 @@ print_repertoire_radar(list_similarity_matrices=list_single_layers,
 # Print classic repertoires comparing max and min locally similar plots for:
 # Simulated murine igh repertoires
 mm_igh<-list()
+#add max locally similar repertoire
 mm_igh[["mm_ig_h_2_0__0_0_0_A"]]<-repertoires_analyzed[["mm_ig_h_2_0__0_0_0_A"]]
+#add min locally similar repertoire
 mm_igh[["mm_ig_h_4_0__0_0_0_A"]]<-repertoires_analyzed[["mm_ig_h_4_0__0_0_0_A"]]
 
 print_repertoire_comparison(list_repertoires=mm_igh,name_plots="mm_igh",aa_freq_length=14,path_figure="figures")
@@ -162,7 +171,9 @@ print_repertoire_comparison(list_repertoires=mm_igh,name_plots="mm_igh",aa_freq_
 
 # Simulated human igh repertoires
 hs_igh<-list()
+#add max locally similar repertoire
 hs_igh[["hs_ig_h_2_0__0_0_0_A"]]<-repertoires_analyzed[["hs_ig_h_2_0__0_0_0_A"]]
+#add min locally similar repertoire
 hs_igh[["hs_ig_h_4_0__0_0_0_A"]]<-repertoires_analyzed[["hs_ig_h_4_0__0_0_0_A"]]
 
 print_repertoire_comparison(list_repertoires=hs_igh,
