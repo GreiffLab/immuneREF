63b8a932640450f3643636e6d17a2addddc375aa
crw-ethz <cedric.r.weber@me.com>
2020-07-21 17:48:49 +0200

    Add files via upload

--- immuneREF_quickstart.R
+++ immuneREF_quickstart.R
@@ -3,7 +3,7 @@ library(immuneREF)
 # Calculate simularity networks for single features (Step 1 and 2)
 similarity_networks <- immuneREF_quickstart(repertoire_list = tutorial_repertoires)
 
-# Calculate network features and plot heatmap of repertoire similarities(Step 4)
-network_features <- analyze_similarity_network(similarity_networks)
+# Calculate network features and plot heatmap of repertoire similarities (condensed network) (Step 4)
+network_features <- analyze_similarity_network(similarity_networks[["Condensed"]])
 
-pheatmap::pheatmap(similarity_networks,scale='row')
+pheatmap::pheatmap(similarity_networks[["Condensed"]],scale='row')
