8ee4551319c10a3d4e1d88ba1f312c10ba0c7114
crw-ethz <cedric.r.weber@me.com>
2021-05-27 15:05:52 +0200

    Update repertoire_overlap.R

--- R/repertoire_overlap.R
+++ R/repertoire_overlap.R
@@ -8,7 +8,7 @@
 #' repertoire_overlap(list_simulated_repertoires=tutorial_repertoires,basis="CDR3_aa")
 #' }
 
-repertoire_overlap <- function(list_simulated_repertoires, basis="CDR3_aa"){
+repertoire_overlap <- function(list_repertoires, basis="CDR3_aa"){
   #calculates the overlap between every pair of repertoires based on either CDR3 aa,nt or VDJ aa,nt. Default (CDR3 a.a.)
   #output: matrix of every pairwise comparison
   intersect_percentage_function <- function(x,y){
@@ -17,23 +17,23 @@ repertoire_overlap <- function(list_simulated_repertoires, basis="CDR3_aa"){
 
 
   #save repnames and make combos
-  rep_names <- names(list_simulated_repertoires)
+  rep_names <- names(list_repertoires)
   name_combo <- combn(rep_names, 2)
 
   #calculate overlaps based on chosen sequence
   #overlap is calculated as intersect(x,y)/min(length(x),length(y))
   if(basis=="CDR3_aa"){
     overlap_tmp <- sapply(split(name_combo, col(name_combo)),
-                          function(x) intersect_percentage_function(unique(as.character(list_simulated_repertoires[[x[1]]]$junction_aa)),unique(as.character(list_simulated_repertoires[[x[2]]]$junction_aa))))
+                          function(x) intersect_percentage_function(unique(as.character(list_repertoires[[x[1]]]$junction_aa)),unique(as.character(list_repertoires[[x[2]]]$junction_aa))))
   }else if(basis == "CDR3_nt"){
     overlap_tmp <- sapply(split(name_combo, col(name_combo)),
-                          function(x) intersect_percentage_function(unique(as.character(list_simulated_repertoires[[x[1]]]$junction)),unique(as.character(list_simulated_repertoires[[x[2]]]$junction))))
+                          function(x) intersect_percentage_function(unique(as.character(list_repertoires[[x[1]]]$junction)),unique(as.character(list_repertoires[[x[2]]]$junction))))
   }else if(basis == "VDJ_aa"){
     overlap_tmp <- sapply(split(name_combo, col(name_combo)),
-                          function(x) intersect_percentage_function(unique(as.character(list_simulated_repertoires[[x[1]]]$sequence_aa)),unique(as.character(list_simulated_repertoires[[x[2]]]$sequence_aa))))
+                          function(x) intersect_percentage_function(unique(as.character(list_repertoires[[x[1]]]$sequence_aa)),unique(as.character(list_repertoires[[x[2]]]$sequence_aa))))
   }else if(basis == "VDJ_nt"){
     overlap_tmp <- sapply(split(name_combo, col(name_combo)),
-                          function(x) intersect_percentage_function(unique(as.character(list_simulated_repertoires[[x[1]]]$sequence)),unique(as.character(list_simulated_repertoires[[x[2]]]$sequence))))
+                          function(x) intersect_percentage_function(unique(as.character(list_repertoires[[x[1]]]$sequence)),unique(as.character(list_repertoires[[x[2]]]$sequence))))
   }else if(basis == "V_CDR3_J_nt"){
     overlap_tmp <- sapply(split(name_combo, col(name_combo)),
                           function(x) intersect_percentage_function(unique(paste(list_repertoires[[x[1]]]$v_call,as.character(list_repertoires[[x[1]]]$junction),list_repertoires[[x[1]]]$j_call,sep="_")),unique(paste(list_repertoires[[x[2]]]$v_call,as.character(list_repertoires[[x[2]]]$junction),list_repertoires[[x[2]]]$j_call,sep="_"))))
@@ -43,7 +43,7 @@ repertoire_overlap <- function(list_simulated_repertoires, basis="CDR3_aa"){
   }
 
   ## initialize matrix (initialize to 1 as diagonal is 100%overlap)
-  overlap_stat <- matrix(1, ncol = length(list_simulated_repertoires), nrow = length(list_simulated_repertoires))
+  overlap_stat <- matrix(1, ncol = length(list_repertoires), nrow = length(list_repertoires))
   ## fill in lower triangle
   overlap_stat[lower.tri(overlap_stat)] <- overlap_tmp
   ## transpose and fill in lower triangle of transposed
